8e328410741502d59a5a0b46bd4a654bd454447f
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-02 23:35:07 +0530

    feat: dynamic array class

---
+++ 2. OOP-1/DynamicArrayUse.java
@@ -0,0 +1,28 @@
+public class DynamicArrayUse {
+    // 1. constructor
+    // 2. add (adds to the end)
+    // 3. size
+    // 4. set
+    // 5. get
+    // 6. remove last
+    // 7. isEmpty
+    public static void main(String[] args) {
+        DynamicArray d = new DynamicArray();
+
+        for (int i = 0; i < 100; i++) {
+            d.add(i + 10);
+        }
+
+        System.out.println(d.size());
+
+        d.set(4, 10);
+        System.out.println(d.get(3));
+        System.out.println(d.get(4));
+
+        while (!d.isEmpty()) {
+            System.out.println(d.removeLast());
+            System.out.println("size = " + d.size());
+        }
+
+    }
+}
