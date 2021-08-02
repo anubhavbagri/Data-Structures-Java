2a70a4ef317cb92077f4d134d0239125c959a673
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-02 16:08:30 +0530

    feat: constructors

--- 2. OOP-1/Student.java
+++ 2. OOP-1/Student.java
@@ -2,6 +2,19 @@ public class Student {
     public String name;
     private int rollNumber;
 
+    // public Student() {
+    // rollNumber = 100;
+    // }
+
+    public Student(String n) {
+        name = n;
+    }
+
+    public Student(String n, int rn) {
+        name = n;
+        rollNumber = rn;
+    }
+
     public void setRollNumber(int rn) {
         if (rn <= 0) {
             return;
@@ -11,5 +24,9 @@ public class Student {
 
     public int getRollNumber() {
         return rollNumber;
+    }
+
+    public void print() {
+        System.out.println(name + " : " + rollNumber);
     }
 }
--- 2. OOP-1/StudentUse.java
+++ 2. OOP-1/StudentUse.java
@@ -3,17 +3,16 @@ import java.util.*;
 public class StudentUse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Student s1 = new Student();
-        s1.name = "Anubhav";
-        s1.setRollNumber(-123);
+        Student s1 = new Student("Anubhav");
+        // System.out.println(s1.name);
+        // System.out.println(s1.getRollNumber());
+        s1.print();
 
-        Student s2 = new Student();
-        s2.name = "Vanshika";
-        s2.setRollNumber(121);
-
-        System.out.println(s1.name);
-        System.out.println(s2.name);
-        System.out.println(s1.getRollNumber());
-        // System.out.println(s1);
+        Student s2 = new Student("Vanshika", 121);
+        s2.print();
+        // System.out.println(s1.name);
+        // System.out.println(s2.name);
+        // System.out.println(s1.getRollNumber());
+        // // System.out.println(s1);
     }
 }
