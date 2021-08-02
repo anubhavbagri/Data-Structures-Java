cae85193d387df494c7d6a0cbcc0146a9232c5ef
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-02 16:33:14 +0530

    feat: final keyword

--- 2. OOP-1/Student.java
+++ 2. OOP-1/Student.java
@@ -1,26 +1,28 @@
 public class Student {
     public String name;
-    private int rollNumber;
+    // final data members can be initialized as soon as they are declared.
+    private final int rollNumber;
 
     // public Student() {
     // rollNumber = 100;
     // }
 
-    public Student(String n) {
-        name = n;
-    }
+    // public Student(String n) {
+    // name = n;
+    // rollNumber = 100;
+    // }
 
     public Student(String n, int rn) {
         name = n;
         rollNumber = rn;
     }
 
-    public void setRollNumber(int rn) {
-        if (rn <= 0) {
-            return;
-        }
-        rollNumber = rn;
-    }
+    // public void setRollNumber(int rn) {
+    // if (rn <= 0) {
+    // return;
+    // }
+    // rollNumber = rn;
+    // }
 
     public int getRollNumber() {
         return rollNumber;
--- 2. OOP-1/StudentUse.java
+++ 2. OOP-1/StudentUse.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class StudentUse {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        Student s1 = new Student("Anubhav");
+        Student s1 = new Student("Anubhav", 123);
         // System.out.println(s1.name);
         // System.out.println(s1.getRollNumber());
         s1.print();
