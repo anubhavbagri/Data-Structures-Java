c10b613c5dd0236962b1d9c0e479ee6809d0f46a
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-03 21:40:01 +0530

    style: info comment

--- 3. OOP-2/VehicleUse.java
+++ 3. OOP-2/VehicleUse.java
@@ -7,12 +7,15 @@ public class VehicleUse {
 
         Vehicle v = new Car(10, 20); // Every Vehicle can be a Car
 
-        v.print();
+        v.print(); // At compile time, it checks that there exists a print function in Vehicle,
+                   // but its actually decided at run time that print() function would be called
+                   // from Car since the object is of Car type.
 
-        Car c = new Car(10, 100); // when we make the object of the child class, it also makes the object of the
+        // Car c = new Car(10, 100); // when we make the object of the child class, it
+        // also makes the object of the
         // parent class
-        c.color = "Black";
-        c.setMaxSpeed(100);
+        // c.color = "Black";
+        // c.setMaxSpeed(100);
         // c.print(); // print method of Car would be called
     }
 }
