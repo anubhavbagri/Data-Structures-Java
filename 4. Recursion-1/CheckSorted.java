16e8dd242e55e594eb65331bca9b6bf627b24e52
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-04 19:28:03 +0530

    feat: better approach to recursion in arrays

--- 4. Recursion-1/CheckSorted.java
+++ 4. Recursion-1/CheckSorted.java
@@ -37,8 +37,28 @@ public class CheckSorted {
         return smallAns;
     }
 
+    // This function now checks if the array is sorted from startIndex to end
+    private static boolean checkSortedBetter(int input[], int startIndex) {
+        if (startIndex >= input.length - 1) {
+            return true;
+        }
+
+        if (input[startIndex] > input[startIndex + 1]) {
+            return false;
+        }
+
+        boolean smallAns = checkSortedBetter(input, startIndex + 1);
+        return smallAns;
+
+    }
+
+    // helper function
+    public static boolean checkSortedBetter(int input[]) {
+        return checkSortedBetter(input, 0);
+    }
+
     public static void main(String[] args) {
         int[] input = { 1, 2, 3 };
-        System.out.println(checkSorted(input));
+        System.out.println(checkSortedBetter(input));
     }
 }
