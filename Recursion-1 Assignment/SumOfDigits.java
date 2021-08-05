b9ea3f605b297abd079a064dd0b6882788389db2
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-08-05 17:33:32 +0530

    feat: problem 5 solution

---
+++ Recursion-1 Assignment/SumOfDigits.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class SumOfDigits {
+
+    public static int getSumOfDigits(int n) {
+        if (n <= 0) {
+            return 0;
+        }
+        return (n % 10) + getSumOfDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.println(getSumOfDigits(N));
+        sc.close();
+    }
+}
