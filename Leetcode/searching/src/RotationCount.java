85e0c983edd3363b74853f24b91648d47aef441e
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-09-20 19:21:52 +0530

    feat: testing with another input

--- Leetcode/searching/src/RotationCount.java
+++ Leetcode/searching/src/RotationCount.java
@@ -1,6 +1,7 @@
 public class RotationCount {
     public static void main(String[] args) {
-        int[] arr = {4, 5, 6, 7, 0, 1, 2, 3};
+//        int[] arr = {4, 5, 6, 7, 0, 1, 2, 3};
+        int[] arr = {0, 1, 2, 3, 4, 5, 6, 7};
         System.out.println(countRotations(arr));
     }
 
