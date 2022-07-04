84a8a411e84c63dedac90ce472d0a1c51bd79275
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2022-07-04 22:55:05 +0530

    OOP2: Packages

---
+++ Object-oriented-programming/src/OOP2/a/Greeting.java
@@ -0,0 +1,10 @@
+package OOP2.a;
+
+import static OOP2.b.Message.message;
+
+public class Greeting {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        message();
+    }
+}
